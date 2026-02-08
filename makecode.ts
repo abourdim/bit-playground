687588511136fdc0d45a11c51f7e1fdc6efa7941
abdelhak <abdelhak.bourdim@gmail.com>
2026-02-08 19:27:59 +0100

    upd

--- makecode.ts
+++ makecode.ts
@@ -1,6 +1,6 @@
 /**
  * =========================================================
- *  micro:bit Playground – BLE Firmware (V3)
+ *  micro:bit Playground – BLE Firmware (V2)
  * =========================================================
  *
  * UART over Bluetooth LE — full telemetry & control
